534e4236019a9e8d93300a1847d6239f5116960a
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-03 22:09:07 +0530

    chore(analytics): register analytics components inside core monitor module wiring

--- src/monitor/monitor.module.ts
+++ src/monitor/monitor.module.ts
@@ -3,11 +3,12 @@ import { MonitorController } from './monitor.controller';
 import { MonitorRepository } from './monitor.repository';
 import { MonitorService } from './monitor.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { AnalyticsService } from './analytics.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [MonitorRepository, MonitorService],
-  controllers: [MonitorController],
+  providers: [MonitorRepository, MonitorService, AnalyticsService],
+  controllers: [MonitorController, AbortController],
   exports: [MonitorService],
 })
 export class MonitorModule {}
