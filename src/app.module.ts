989709001c2051b651a5ddb3f84d6d7259c47080
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-04 10:55:13 +0530

    chore(notification): enable global event emitter and register notification module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { MonitorModule } from './monitor/monitor.module';
 import { MonitorEngineModule } from './monitor-engine/monitor-engine.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [PrismaModule, MonitorModule, MonitorEngineModule],
+  imports: [
+    PrismaModule,
+    EventEmitterModule.forRoot({
+      wildcard: false,
+      delimiter: '.',
+      maxListeners: 10,
+    }),
+    MonitorModule,
+    MonitorEngineModule,
+  ],
   controllers: [],
   providers: [],
 })
