696a38c1cd5d7609ae733600a17fceb14d26dc9e
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-04 14:06:00 +0530

    chore(auth): format RegisterDto

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,13 +1,13 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class RegisterDto{
-  @IsEmail({},{message: 'Invalid email address'})
+export class RegisterDto {
+  @IsEmail({}, { message: 'Invalid email address' })
   @IsNotEmpty()
   email: string;
 
   @IsString()
   @IsNotEmpty()
-  @MinLength(8, {message: 'Password must be at least 8 characters long'})
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
 
   @IsString()
