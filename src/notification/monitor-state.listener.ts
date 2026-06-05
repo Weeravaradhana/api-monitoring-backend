5cb3bc95db0952e7c683236d99fb857f67dc8d74
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-05 13:09:49 +0530

    refactor(listeners): unify monitor up/down event payload structure

--- src/notification/monitor-state.listener.ts
+++ src/notification/monitor-state.listener.ts
@@ -11,18 +11,15 @@ export class MonitorStateListener {
   constructor(private readonly notificationService: NotificationService) {}
 
   @OnEvent('monitor.down', { async: true })
-  handleMonitorDownEvent(payload: {
-    monitorId: string;
-    url: string;
-    statusCode: number | null;
-    errorMessage: string | null;
-  }) {
+  async handleMonitorDownEvent(
+    payload: monitorEventPayloadInterface.MonitorEventPayload,
+  ) {
     this.logger.log(
       `Received 'monitor.down' event for Monitor ID: ${payload.monitorId}. Processing alert...`,
     );
 
-    this.notificationService.sendFailureAlert(
-      payload.monitorId,
+    await this.notificationService.sendFailureAlert(
+      payload.userEmail,
       payload.url,
       payload.statusCode,
       payload.errorMessage,
@@ -30,13 +27,16 @@ export class MonitorStateListener {
   }
 
   @OnEvent('monitor.up', { async: true })
-  handleMonitorUpEvent(
+  async handleMonitorUpEvent(
     payload: monitorEventPayloadInterface.MonitorEventPayload,
   ) {
     this.logger.log(
       `Received 'monitor.up' event for [${payload.name}]. Dispatching recovery alert...`,
     );
 
-    this.notificationService.sendRecoveryAlert(payload.monitorId, payload.url);
+    await this.notificationService.sendRecoveryAlert(
+      payload.monitorId,
+      payload.url,
+    );
   }
 }
