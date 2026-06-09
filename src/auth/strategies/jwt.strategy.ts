41e31c79f81062a5e812485f136fd4e684618f87
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-09 22:37:52 +0530

    feat: extract JWT from cookies instead of Authorization header

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-jwt';
 import { JwtPayload } from '../interface/jwt-payload.interface';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor() {
     super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      jwtFromRequest: (req: Request) => {
+        if (req && req.cookies) {
+          return (req.cookies['accessToken'] as string) || null;
+        }
+        return null;
+      },
       ignoreExpiration: false,
       secretOrKey: process.env.JWT_ACCESS_SECRET!,
     });
