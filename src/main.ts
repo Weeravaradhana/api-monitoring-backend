15a9daba43bb335547d1d0f516770f6dd829276d
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-20 10:57:48 +0530

    `feat: enable WebSocket adapter and allow OPTIONS in CORS config`

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.useWebSocketAdapter(new IoAdapter(app));
   app.enableCors({
     origin: 'http://localhost:3001',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
 
